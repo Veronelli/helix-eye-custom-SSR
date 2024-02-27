4653f7c4c4828c383aa967469ac693322fe87e6a
Veronelli <facu2000veronelli@outlook.com>
2024-02-27 09:39:11 -0300

    config render from server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello World from server")
+import express from 'express';
+import type { Express, Request, Response } from 'express';
+import {config} from './config/index';
+
+import { template } from './render/template';
+import { render } from './render';
+
+const app: Express = express();
+
+app.use(express.static('dist'));
+
+app.get("/",(req: Request,res: Response)=>{
+    res.send(render(req.url))
+})
+
+app.listen(config.PORT,()=>{
+    console.log("Listening on Port ", config.PORT )
+})
